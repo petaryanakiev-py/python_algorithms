075f5e01e4607fbbdc3b51dc62d07a195df55f54
petar98 <petaryanakiev.py@gmail.com>
2020-08-02 00:07:23 +0300

    graphs representations created

--- fibonacci.py
+++ fibonacci.py
@@ -8,6 +8,16 @@ def factorial(number):
         return 1
     return number * factorial(number - 1)
 
+def fib(position):
+    x = 0
+    y = 0
+    z = 0
+    for _ in range(position):
+        z = x + y
+        x = y
+        y = z
+    return z
+
 # Test Cases
 print(get_fib(3))
 print(get_fib(9))
